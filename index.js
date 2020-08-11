b33b6a05ef6091e0c17bd622b939053a82beb350
Huynh Tran Phu <huynhtranphu05121999@gmail.com>
2020-08-12 00:27:29 +0700

    Create user validation

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
+var  userRoute = require('./routes/user.route');
 var port = 3000
-app.get('/',function(request,response){
-    response.send('Hello world');
+
+app.set('view engine','pug');
+app.set('views','./views');
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'))
+
+app.get('/',function(req,res){
+    res.render('index');
 });
-app.get('/phu',function(request,response){
-    response.send('Hello phu');
-});
+app.use('/users',userRoute);
 app.listen(port,function(){
     console.log('Server listening on port '+port );
 });
